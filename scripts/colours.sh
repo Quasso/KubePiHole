fee871eff0751ca255e4bf61c09c2d05b8a47f09
Quasso <alex@forbettergood.ge>
2022-08-05 23:48:21 +0100

    update, if this doesn't work I'm leaving it for now!

--- scripts/colours.sh
+++ scripts/colours.sh
@@ -1,24 +1,25 @@
 # Enable colours
-export TERM=xterm-color
-export GREP_OPTIONS='--color=auto' GREP_COLOR='1;32'
+export TERM=xterm-COLOR
+export GREP_OPTIONS='--COLOUR=auto' GREP_COLOR='1;32'
 export CLICOLOR=1
 export LSCOLORS=ExFxCxDxBxegedabagacad
 
 # Colour definitions
-export COLOUR_NC='\e[0m' # reset
-export COLOUR_BLACK='\e[0;30m'
-export COLOUR_GRAY='\e[1;30m'
-export COLOUR_RED='\e[0;31m'
-export COLOUR_LIGHT_RED='\e[1;31m'
-export COLOUR_GREEN='\e[0;32m'
-export COLOUR_LIGHT_GREEN='\e[1;32m'
-export COLOUR_BROWN='\e[0;33m'
-export COLOUR_YELLOW='\e[1;33m'
-export COLOUR_BLUE='\e[0;34m'
-export COLOUR_LIGHT_BLUE='\e[1;34m'
-export COLOUR_PURPLE='\e[0;35m'
-export COLOUR_LIGHT_PURPLE='\e[1;35m'
-export COLOUR_CYAN='\e[0;36m'
-export COLOUR_LIGHT_CYAN='\e[1;36m'
-export COLOUR_LIGHT_GRAY='\e[0;37m'
-export COLOUR_WHITE='\e[1;37m'
+export C_RESET='\033[0m'
+
+export COLOUR_BLACK='\033[0;30m'
+export COLOUR_GRAY='\033[1;30m'
+export COLOUR_RED='\033[0;31m'
+export COLOUR_LIGHT_RED='\033[1;31m'
+export COLOUR_GREEN='\033[1;32m'
+export COLOUR_LIGHT_GREEN='\033[1;32m'
+export COLOUR_BROWN='\033[0;33m'
+# export COLOUR_YELLOW='\033[1;33m'
+export COLOUR_BLUE='\033[0;34m'
+export COLOUR_LIGHT_BLUE='\033[1;34m'
+export COLOUR_PURPLE='\033[0;35m'
+export COLOUR_LIGHT_PURPLE='\033[1;35m'
+export COLOUR_CYAN='\033[0;36m'
+export COLOUR_LIGHT_CYAN='\033[1;36m'
+export COLOUR_LIGHT_GRAY='\033[0;37m'
+export COLOUR_WHITE='\033[1;37m'

757a8aa8d04ccc9c296370c193776c1dbe07d309
Quasso <alex@forbettergood.ge>
2022-08-06 00:19:39 +0100

    set up the barebones for "deploy" cli function

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export PRINT_PREFIX="KubePiHole DEPLOY"
+
+# The function that maps and deploys all the necessary k8s manifests!
+
+function deploy_pihole() {
+    print_assistant "Deploy Pi-hole function is now running!"
+}

2958730846648d2c994ae8cdabb4ea004caa88a2
Quasso <alex@forbettergood.ge>
2022-08-05 22:16:46 +0100

    fix the source and also add a placeholder inside the install_dependencies function to mitigate cli error

--- scripts/install-required-packages.sh
+++ scripts/install-required-packages.sh
@@ -3,5 +3,5 @@
 # A helper function to install the necessary packages (dependencies) on a fresh Raspberry Pi OS install
 
 function install_dependencies() {
-    
+    echo "Installing the necessary packages for running a Kubernetes cluster..."
 }

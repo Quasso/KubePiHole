70432604c409216b7156966352cdd0f0c7456b69
Quasso <alex@forbettergood.ge>
2022-08-06 00:54:42 +0100

    try another method with APT, should be better

--- scripts/install-required-packages.sh
+++ scripts/install-required-packages.sh
@@ -2,30 +2,41 @@
 
 export PRINT_PREFIX="KubePiHole Install Deps"
 
+#
+#
 # A helper function to install the necessary packages (dependencies) on a fresh Raspberry Pi OS install
-
+# Ref: https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+# From: https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/#install-using-native-package-management
+#
+#
 function install_dependencies() {
     print_assistant "Ensuring your package repositories are up-to-date..."
     sudo apt-get update # needs to run as sudo on Raspberry Pi OS because there is no default root user...
-    
-    print_assistant "Moving to \$HOME directory for installation ($HOME)..."
-    cd $HOME
+
+    # print_assistant "Moving to \$HOME directory for installation ($HOME)..."
+    # cd $HOME
 
     print_assistant "Installing the necessary packages for running a Kubernetes cluster..."
 
-    print_assistant "1.1) Download kubectl..."
-    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    print_assistant "1.1) Install pre-reqs for kubectl via apt-get..."
+    # curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    sudo apt-get install -y ca-certificates curl
+    sudo apt-get install -y apt-transport-https
 
-    print_assistant "1.2) Validate the binary (optional but good)..."
+    # print_assistant "1.2) Validate the binary (optional but good)..."
 
-    print_assistant "1.2.1) Pull the sha256..."
-    curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+    print_assistant "1.2) Pull the apt-key..."
+    # curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+    sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
-    print_assistant "1.2.2) Verify the sha256..."
-    echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check # TODO: add handler if the above command outputs FAILED
+    print_assistant "1.3) Add the apt repository..."
+    # echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check # TODO: add handler if the above command outputs FAILED
+    echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
     print_assistant "1.3) Install kubectl"
-    chmod +x kubectl
-    mkdir -p ~/.local/bin
-    mv ./kubectl ~/.local/bin/kubectl
+    sudo apt-get update
+    sudo apt-get install -y kubectl
+    # chmod +x kubectl
+    # mkdir -p ~/.local/bin
+    # mv ./kubectl ~/.local/bin/kubectl
 }

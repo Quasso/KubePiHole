ccc66de95bc8ceb84a2dfe68d6dfad6d8dd88ba8
Quasso <alex@forbettergood.ge>
2022-08-06 00:35:03 +0100

    oops local must be inside function duh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,13 @@
 
 export PRINT_PREFIX="KubePiHole DEPLOY"
 
-# Kubernetes template files
-local K8S_STORAGECLASS="/kube/templates/kube-pihole-storageclass.yaml"
-local K8S_PVCS="/kube/templates/kube-pihole-pvc.yaml"
-local K8S_PVS="/kube/templates/kube-pihole-pv.yaml"
-
 # The function that maps and deploys all the necessary k8s manifests!
 function deploy_pihole() {
     PIHOLE_NAME="kube-pihole"
+    # Kubernetes template files
+    local K8S_STORAGECLASS="/kube/templates/kube-pihole-storageclass.yaml"
+    local K8S_PVCS="/kube/templates/kube-pihole-pvc.yaml"
+    local K8S_PVS="/kube/templates/kube-pihole-pv.yaml"
 
     print_assistant "Deploy Pi-hole function is now running!" "title"
 

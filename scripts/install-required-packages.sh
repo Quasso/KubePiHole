850166f0ab86a9637b78017039426f354d3ab953
Quasso <alex@forbettergood.ge>
2022-08-06 01:05:19 +0100

    tidy up install-required

--- scripts/install-required-packages.sh
+++ scripts/install-required-packages.sh
@@ -10,33 +10,19 @@ export PRINT_PREFIX="KubePiHole Install Deps"
 #
 #
 function install_dependencies() {
-    # print_assistant "Ensuring your package repositories are up-to-date..."
-    # sudo apt-get update # needs to run as sudo on Raspberry Pi OS because there is no default root user...
-
-    # print_assistant "Moving to \$HOME directory for installation ($HOME)..."
-    # cd $HOME
-
     print_assistant "Installing the necessary packages for running a Kubernetes cluster..."
 
-    print_assistant "1.1) Install pre-reqs for kubectl via apt-get..."
-    # curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    print_assistant "1) Install pre-reqs for kubectl via apt-get..."
     sudo apt-get install -y ca-certificates curl
     sudo apt-get install -y apt-transport-https
 
-    # print_assistant "1.2) Validate the binary (optional but good)..."
-
     print_assistant "1.2) Pull the apt-key..."
-    # curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
     sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 
     print_assistant "1.3) Add the apt repository..."
-    # echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check # TODO: add handler if the above command outputs FAILED
     echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-    print_assistant "1.3) Install kubectl"
+    print_assistant "2) Install kubectl"
     sudo apt-get update
     sudo apt-get install -y kubectl
-    # chmod +x kubectl
-    # mkdir -p ~/.local/bin
-    # mv ./kubectl ~/.local/bin/kubectl
 }

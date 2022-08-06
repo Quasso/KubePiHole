96500200dcf34b87a85d683d1ef29ca3861004a9
Quasso <alex@forbettergood.ge>
2022-08-06 04:35:29 +0100

    change dir ref

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,9 +6,9 @@ export PRINT_PREFIX="KubePiHole DEPLOY"
 function deploy_pihole() {
     PIHOLE_NAME="kube-pihole"
     # Kubernetes template files
-    local K8S_STORAGECLASS="/kube/templates/kube-pihole-storageclass.yaml"
-    local K8S_PVCS="/kube/templates/kube-pihole-pvc.yaml"
-    local K8S_PVS="/kube/templates/kube-pihole-pv.yaml"
+    local K8S_STORAGECLASS="./kube/templates/kube-pihole-storageclass.yaml"
+    local K8S_PVCS="./kube/templates/kube-pihole-pvc.yaml"
+    local K8S_PVS="./kube/templates/kube-pihole-pv.yaml"
 
     print_assistant "Deploy Pi-hole function is now running!" "title"
 

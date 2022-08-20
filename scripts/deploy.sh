e94913693d365281c92cf9d8a207974ccfd691db
Quasso <alex@forbettergood.ge>
2022-08-20 23:45:59 +0100

    with a short 5s pause before binding port it worked first time, may need improving but for now I'll assume it's OK

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -43,6 +43,10 @@ function deploy_pihole() {
     echo
     echo "Change it immediately. Happy blocking!"
 
+    echo
+    echo "Sleeping for 5 seconds before trying port-bind..."
+    sleep 5
+
     space_terminal_lg
     echo "Forwarding the port so you can access the web UI locally..."
     kubectl port-forward service/pihole-ui-svc 8000:80
